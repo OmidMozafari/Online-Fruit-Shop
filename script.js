f6a6e8dd8f4f3c30d4df0fa94972fa518168b0e0
OmidMozafari <omidmozafari444@gmail.com>
2026-03-15 15:45:41 +0430

    Second commit. Wrote some codes, added features to the files and also styles.

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ const message = document.getElementById("message")
 const total = document.getElementById("total")
 const finalList = document.getElementById("finalList")
 const finalTotal = document.getElementById("finalTotal")
+const addToCart = document.getElementById("addToCart")
+const stopPurchasing = document.getElementById("stopPurchasing")
+const small = document.querySelector("#purchasing small")
+
+// show error function
+function ShowError(){
+    small.style.color = "red"
+    small.style.visibility = "visible"
+}
 
 let products = [
-    {name: "milk", price: 35},
+    {name: "fig", price: 80},
     {name:"apple", price: 50},   
     {name:"mongo", price: 270},   
     {name:"banana", price: 150},   
@@ -31,4 +40,49 @@ for (p of products){
         <span class="currency">AFN</span>
         </li>`
 }
+
+// Adding to cart function
+function cartAdding(){
+
+    let totalPrice = 0 
+
+    let choosenProductArray = []
+
+    choosenProduct = productName.value.split(" ");
+
+    for(b of choosenProduct){
+
+        let upperWord = b.charAt(0).toUpperCase()
+        let lowerWord = b.slice(1).toLowerCase()
+        let finalWord = upperWord + lowerWord
+
+        choosenProductArray.push(finalWord)
+
+    }
+    let finalChoosenName = choosenProductArray.join(" ")
+
+    quantity = productQuantity.value
+
+    for(c of products){
+
+        if(finalChoosenName === c.name){
+
+            totalPrice = c.price * quantity
+            console.log(totalPrice);
+            
+
+        }else{
+            ShowError
+        }
+    }
+    
+}
+
+
+
+
+
+//events section
+addToCart.addEventListener("click", cartAdding)
+// stopPurchasing.addEventListener("click", )
 
